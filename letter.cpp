0a99075c23258e9f6240e26552648be2d0b84eb8
FlowNeb <55406768+FlowNeb@users.noreply.github.com>
2019-09-23 20:30:29 +0000

    Add files via upload

---
+++ letter.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+cout << "Enter the name of the person you want to write to\n";
+string name;                                
+cin >> name;         
+cout<< "Dear "<<name<<", How are you? I am fine. I miss you. How is school going? Right now i am writing a program\n";
+
+cout << "Enter one of our friend name\n";
+string friends;                                
+cin >> friends;
+cout<< "Have you seen "<<friends<<" lately?\n";
+
+return 0;
+}
