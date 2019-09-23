7e94b38383d0e28d07d729570ffd1b20b6bd4cb1
FlowNeb <55406768+FlowNeb@users.noreply.github.com>
2019-09-23 21:14:23 +0000

    Add files via upload

---
+++ switch with differenc.cpp
@@ -0,0 +1,22 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int a;
+int b;
+
+cout<<"Please give me 2 number:\n";
+cin>>a>>b;
+cout<<"First number:"<<a<<"\n";
+cout<<"Second number:"<<b<<"\n";
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+cout<<"After switch:\n";
+cout<<"First number:"<<a<<"\n";
+cout<<"Second number:"<<b<<"\n";
+
+return 0;
+}
